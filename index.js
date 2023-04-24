a1c8b2b44581158754c7d414b5cc19eac738e9f5
Kristinn <kris@kristinns-air.attlocal.net>
2023-04-24 19:39:12 -0400

    update file structure

--- index.js
+++ index.js
@@ -20,3 +20,45 @@ function writeVideoDataToFile(data) {
     const json = JSON.stringify(data);
     fs.writeFileSync(videosFilePath, json);
 }
+
+app.get('/videos', (req, res) => {
+        const data = readVideoDataFromFile();
+        const videos = data.videos.map(video => {
+        return {
+            id: video.id,
+            title: video.title,
+            channel: video.channel,
+            image: video.image
+        };
+        });
+        res.json(videos);
+    });
+    
+    app.get('/videos/:id', (req, res) => {
+        const id = req.params.id;
+        const data = readVideoDataFromFile();
+        const video = data.videos.find(video => video.id === id);
+        if (!video) {
+        res.status(404).send('Video not found');
+        } else {
+        res.json(video);
+        }
+    });
+    
+    app.post('/videos', (req, res) => {
+        const data = readVideoDataFromFile();
+        const video = {
+        id: Math.random().toString(36).substr(2, 9),
+        title: req.body.title,
+        channel: 'My Channel',
+        image: 'Upload-video-preview.jpg',
+        description: req.body.description
+        };
+        data.videos.push(video);
+        writeVideoDataToFile(data);
+        res.json(video);
+    });
+    
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
