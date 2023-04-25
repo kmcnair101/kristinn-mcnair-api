27c4c87295b7a3a72bda4db3f3ce6a7493cd58de
Kristinn <kris@kristinns-air.attlocal.net>
2023-04-24 21:09:40 -0400

    edit env and deletr excess code

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const videoRouter = require('./routes/videos');
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use('/videos', express.static('public/images'));
 app.use(express.json());
 
-app.use('/videos', videoRouter);
+app.use('/api', videoRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}...`);
--- routes/videos.js
+++ routes/videos.js
@@ -1,10 +1,8 @@
-const express = require('express');
-const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -21,44 +19,64 @@ function writeVideoDataToFile(data) {
     fs.writeFileSync(videosFilePath, json);
 }
 
+function validateVideoData(data) {
+    const { title, description } = data;
+    if (!title || !description) {
+        throw new Error('Title and description are required');
+    }
+    if (title.length > 100 || description.length > 500) {
+        throw new Error('Title cannot be longer than 100 characters, and description cannot be longer than 500 characters');
+    }
+}
+
 app.get('/videos', (req, res) => {
-        const data = readVideoDataFromFile();
-        const videos = data.videos.map(video => {
+    const data = readVideoDataFromFile();
+    const videos = data.videos.map(video => {
         return {
             id: video.id,
             title: video.title,
             channel: video.channel,
             image: video.image
         };
-        });
-        res.json(videos);
     });
-    
-    app.get('/videos/:id', (req, res) => {
-        const id = req.params.id;
-        const data = readVideoDataFromFile();
-        const video = data.videos.find(video => video.id === id);
-        if (!video) {
+    res.json(videos);
+});
+
+app.get('/videos/:id', (req, res) => {
+    const id = req.params.id;
+    const data = readVideoDataFromFile();
+    const video = data.videos.find(video => video.id === id);
+    if (!video) {
         res.status(404).send('Video not found');
-        } else {
+    } else {
         res.json(video);
-        }
-    });
-    
-    app.post('/videos', (req, res) => {
+    }
+});
+
+app.post('/videos', (req, res) => {
+    try {
+        validateVideoData(req.body);
         const data = readVideoDataFromFile();
         const video = {
-        id: Math.random().toString(36).substr(2, 9),
-        title: req.body.title,
-        channel: 'My Channel',
-        image: 'Upload-video-preview.jpg',
-        description: req.body.description
+            id: Math.random().toString(36).substr(2, 9),
+            title: req.body.title,
+            channel: 'My Channel',
+            image: req.body.image,
+            description: req.body.description
         };
         data.videos.push(video);
         writeVideoDataToFile(data);
         res.json(video);
-    });
-    
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Something went wrong');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
